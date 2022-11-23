4007d7b151731552d9bac63542174be06f577b58
sauravgawande <gawandesaurav02@gmail.com>
2022-11-23 07:10:30 +0530

    Done

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React, {Component, useState} from "react";
 import "./../styles/App.css";
+import ButtonComponent from "./ButtonComponent";
 
 function App() {
   return (
     <div id="main">
       // Do not alter the main div
+      <ButtonComponent/>
     </div>
   );
 }
---
+++ src/components/ButtonComponent.js
@@ -0,0 +1,23 @@
+import React,{useState} from 'react'
+
+function ButtonComponent() {
+
+    const [count,setCount] = useState(false);
+    
+    let click=()=>{
+        return ()=>setCount(!count)
+    }
+  return (
+    <div>
+
+          {count ? <p>
+              Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy
+          </p> : null}
+        <button onClick={click}>
+            click
+        </button>
+    </div>
+  )
+}
+
+export default ButtonComponent
